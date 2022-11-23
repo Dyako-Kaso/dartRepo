a15d7b6b42b9d5cf8677fdfa0a5455f9f5009a01
unknown <dkqu180095@uniq.edu.iq>
2022-11-23 12:28:44 +0300

    Basic Dart Numerical Methods to test the values

--- bin/dart_prog.dart
+++ bin/dart_prog.dart
@@ -6,97 +6,33 @@ void main(List<String> arguments) {
    * Year : 4
    */
 
-  // declaring strings , integers and double values
-  String name = "Tom Cruise";
-  int age = 23;
-  double salary = 21.22;
-
-  // (num) datatype is the parent of both (int) and (double) and it can take both (int) and (double) values
-  num luckyNumber = 12.43;
-  luckyNumber = 24;
-
-  // using (string interpolation) to output the values within the quotation marks in strings ''
-  print(
-      'Hello world: my name is ${name} and my age is ${age} and my salary is ${salary} and my lucky number is ${luckyNumber}\n\n');
-
   /**
-   * when a numerical value is concatenated with a string you must use .toString() method to use the numerical value's
-   * string representation otherwise it will cause an error as a numerical value cannot be concatenated with
-   * a string value.
-   */
-  print(
-      'As I said my name is ${name + ' my age is ' + age.toString() + ' my salary is ' + salary.toString() + ' hope it makes sense\n\n'}');
-
-  // many mathematical operations could be done on numerical values when they are outputted within a string
-
-  /**
-   * const values are used when we want a value to remain the same throughout the execution of the program
-   * (const) datatype is used when we want to make a variable or an object immutable meaning that the object
-   * or the variable will not be allowed to take any other values or to add any values to it in the case of
-   * data structures like lists (we will cover them later)
-   * it's value will not change and will not accept new values to be assigned to it
-   * however it must be initialized once declared
-   */
-  const incomeTax = 0.2;
-  double actualSalary = 250;
-
-  print("My actual salary is ${actualSalary} USD and the income tax rate is ${incomeTax} hence my salary " +
-      "after the income tax would become ${actualSalary - (actualSalary * incomeTax)}\n\n");
-
-  // final variables are like (const) but you can assign values to them on a new line and not on the declaration line
-
-  final finalVar;
-  finalVar = 44;
-  print("final variable's value is ${finalVar}\n\n");
-
-  // the dynamic data type is not very recommended during the development process as it can take any kind of values
-
-  dynamic dynVar = "This is a dynamic variable string\n\n";
-  print(dynVar);
-
-  dynVar = 55.4;
-  print("dynamic variable's value is : ${dynVar}\n\n");
-
-  /**
-   * (var) datatype is used for type inference in dart programming language meaning that the dart language
-   * will know what is the data type of the (var) variable based on the value it is assigned
-   * (var) data type must be initialized upon it's declaration and after a specifci data type is assigned to the
-   * variable it can be re-assigned other values BUT!!!! must be of the same data type
-   * var num = 5;
-   * num = 'hello world'; // not acceptable as the (num) variable has been initialized with an (int) value
+   * The numerical Data types have multiple boolean methods that check the type of values
+   * that each one of those numerical variables hold within them and can be used in debugging
+   * processes and other general conditions
    */
 
-  var varVariable = "Hello World";
-  varVariable = "I hope everything is well\n\n";
-  print(varVariable);
+  // the (.isEven) method is used for checking if the numerical value is even or not
+  int num1 = 5;
+  print(num1.isEven);
 
-  /**
-   *  (bool) datatype is used for boolean conditions, when the program requires the (true) or (false) logic
-   *  boolean variables can also be assigned the result of a comparison or condition such as equal to or not equal to
-   */
+  // the (.isOdd) method is used for checking if the numerical value is odd
+  int num2 = 5;
+  print(num2.isOdd);
 
-  bool boolVariable = true;
-  if (boolVariable == true) {
-    print("The bool variable's value is equal to true\n\n");
-  }
-  // the condition returns (false) and is then assigned to the bool variable
-  bool conditionAssignment = 5 == 10;
-  print("The result of the boolean condition assignment is " +
-      conditionAssignment.toString() +
-      "\n\n");
+  // the (.isFinite) method is used for checking if the numerical value is finite or not
+  int num3 = 20;
+  print(num3.isFinite);
 
-  /**
-   * When a variable is suspected to have a null value within the program and might be initialized at all but
-   * it is outputted in a method (function) or printed somewhere in the program it might cause an error or exception
-   * hence it is recommended to use the null safe operator (?) in order to return an explicit (null) value otherwise
-   * the program will not know what to return if the null safe operator is not used and the variable does
-   * not contain any type of value.
-   *
-   * ex.
-   * String email;
-   * print(email);
-   */
+  // the (.isInfinite) method is used for checking if the numerical value is infinite
+  int num4 = 500;
+  print(num4.isInfinite);
 
-  String? email;
-  print(email?.length);
+  // the (.isNaN) method is used for checking if the numerical value is actually a number or not
+  int num5 = 6;
+  print(num5.isNaN);
+
+  // the (.isNegative) method is used for checking if the numerical value is negative or not
+  int num6 = -76;
+  print(num6.isNegative);
 }
