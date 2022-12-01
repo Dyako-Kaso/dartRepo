4bd6687764df5590055bc6ecfdaee63e9e3e5d87
Dyako Kaso <dkqu180095@uniq.edu.iq>
2022-12-01 11:12:06 +0300

    ListMethod-ConditionsInLists-and-MergingLists

--- bin/dart_prog.dart
+++ bin/dart_prog.dart
@@ -9,94 +9,43 @@ void main(List<String> arguments) {
    */
 
   /**
-   * Dart Programming Language offers many data structures that can be used to store data for different
-   * purposes each one of them used for their own distinct purposes.
+   * Dart Programming Language offers many operations that can be conducted on lists. The flexibility
+   * of Dart programming language allows (if-else , for loops) to be used inside list elements for
+   * conditional value assignment.
    */
 
-  /**
-   * Lists are the first data structure in dart programming language that can be seen as a replacement of arrays
-   * lists can be (appended) values as well as getting their lengths and lists can be cleared (emptied) using the
-   * .clear() method which is a part of the many methods that can be done on lists in dart programming language
-   * Lists are declared using the (List) keyword, it's values are placed within brackets []
-   */
+  // in order to perform a function or operation on every element in a list use (.map() ) method and use .toList() to return as list
+  List<String> list1 = ["Diako", "Muhammed", "Hala"];
+  final elementLenghts = list1.map((e) => e.length).toList();
+  print(elementLenghts);
 
-  // lists can be declared explicitely using the keyword (List)
-  List list1 = [1, 2, 3];
-  print(list1);
+  // in order to perform a condition on every list element you can use (.where() ) , returns strings that have less than or equal to 10 characters
+  List<String> list2 = ["Tom Cruise", "Anna De Armas", "Shwan Atuf"];
+  final lessThanTen = list2.where((element) => element.length <= 10).toList();
+  print(lessThanTen);
 
-  // lists can also be declared explicitely for a specific datatype using List <datatype>
-  List<String> listOfStrings = [
-    "Software Engineering",
-    "Network Security",
-    "Information Technology"
+  // using an efficient for loop inside the list elements
+  final listToOutput = <String>["Diako", "Kaso"];
+  List<String> list3 = [for (final element in listToOutput) element, "Hello"];
+  print(list3);
+
+  // using conditions to assign values to list elements
+  bool condition = true;
+  List<String> list4 = [
+    if (condition) "It's True" else "It's False",
+    "Anyways"
   ];
-  print(listOfStrings);
+  print(list4);
 
-  /**
-   * lists can also be declared implicitely using the (var) and the (final) keywords as one of the capabilties of
-   * dart programming language it type-inference , meaning that dart programming language based on the values
-   * assigned to a variable will know which type of variable it is
-   */
-  final impList1 = [4, 6, 8];
-  print(impList1);
-
-  // lists can be declared implicitely for a specific data type.
-  final impList2 = <String>["Hello", "Hi", "Hey"];
-  print(impList2);
-
-  // in order to know how many elements are inside of a list you can use the (.length()) method
-  final impList3 = <int>[6, 5, 4, 3, 2, 1];
-  print(impList3.length);
-
-  /**
-   * MAP is another data structure that is offered in dart programming language , it maps keys to values
-   * you can declare specific data types for the (keys) and the (values) using Map <int , String> = {'key':'value'}
-   * The difference between Maps and lists in declaration is the usage of brackets [] and curly braces {}
-   */
-
-  // you can explicitely declare maps using the (Map) keyword
-  Map<int, String> map1 = {1: "Diako", 2: "Hala", 3: "Parza"};
-
-  /**
-   * Best way to iterate through data structures like Maps and Lists and Sets is the usage of (For) loops
-   * fortunately the syntax of the (for) loop in dart programming language is similar to the other object
-   * oriented programming language like (Java) and (C++)
-   */
-
-  for (int i = 1; i <= map1.length; i++) {
-    print(map1[i]);
-  }
-
-  // you can also declare Maps implicitely with (var) and (final) variable
-  final map2 = <dynamic, dynamic>{
-    1: "Software Engineering",
-    2: "BioMedical Engineering",
-    3: "Human Resource Development"
-  };
-
-  // forEach loop is a very efficient loop to use with Maps as it outputs both the (key) and (value) with ease
-  map2.forEach((key, value) {
-    print('key : ${key} , value : ${value}');
-  });
-
-  /**
-   * Sets are another data structure that is similar to Maps in terms of declaration but it only takes in one element at a time
-   * repetition in sets get ignored when the values are outputted or the (.length) method is used it does not count duplicate values
-   * sets are declared as   Set <int> setName = {1,2,3,4}    with curly braces
-   */
-
-  Set<int> set1 = {1, 2, 2, 3};
-  set1.forEach((element) {
+  // you can use the .forEach() loop in order to output the list elements
+  List<String> list5 = ["Maryam", "Hannah", "Sima", "Karzan"];
+  list5.forEach((element) {
     print(element);
   });
 
-  print("---------------------Set2------------------------");
-
-  // you can declare Sets with the (final) and the (var) keywords
-  final set2 = <int>{5, 6, 7, 8, 8};
-  set2.forEach((element) {
-    print(element);
-  });
-
-  print("The length of Set 2 is ${set2.length}");
+  // in order to merge two or more lists you can use the expansion operator (...)
+  List<String> mergeList1 = ["Hello", "Word"];
+  List<String> mergeList2 = ["Software Engineering", "Biomedical Engineering"];
+  List<String> mergeList3 = [...mergeList1, ...mergeList2];
+  print(mergeList3);
 }
